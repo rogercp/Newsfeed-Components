3ca96e4be3e6500cfeeb3823edf615071df73b90
Roger Caballero <rogercp1234@yahoo.com>
2019-04-10 14:15:41 -0700

    text expands

--- Article/Article.js
+++ Article/Article.js
@@ -14,15 +14,17 @@ class Article {
     
     // Set a click handler on the expandButton reference, calling the 
     //expandArticle method.
-   
+   this.expandButton.addEventListener("click",()=>this.expandArticle());
     
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or 
     //hide the article.
-    
-
+    this.expandButton.classList.toggle("close");
+    this.expandButton.classList.toggle("article-open");
+    this.article.classList.toggle("article-open");
+    this.expandButton.classList.toggle("expandButton");
   }
 }
 
